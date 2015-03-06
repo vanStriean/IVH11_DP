41ccc514dc89f4ac17ad49a6d5143082cf6e2752
Imkr <Imkr@145.102.75.139>
2015-03-06 14:28:17 +0100

    proxy pattern toegevoegd

---
+++ TemplatePattern/src/proxypattern/ProxyPatternDemo.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proxypattern;
+
+/**
+ *
+ * @author Imkr
+ */
+public class ProxyPatternDemo {
+	
+   public static void main(String[] args) {
+      Image image = new ProxyImage("test_10mb.jpg");
+
+      //image will be loaded from disk
+      image.display(); 
+      System.out.println("");
+      
+      //image will not be loaded from disk
+      image.display(); 	
+   }
+}
